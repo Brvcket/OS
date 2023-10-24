ea680d425fba05dfe6978e555617a1ad1a152cc1
Bulat Akhmatov <b.akhmatov@innopolis.university>
2023-10-24 18:43:48 +0300

    ex2.c chunks fix

--- week07/ex2.c
+++ week07/ex2.c
@@ -1,61 +1,97 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/mman.h>
+#include <ctype.h>
 
-#define FILE_SIZE 500 * 1024 * 1024
+#define MAX_FILE_SIZE 500 * 1024 * 1024
 #define BUFFER_SIZE 500 * 1024
 
 int main() {
-    int text = open("text.txt", 0102, 0644);
+    FILE *text_file = fopen("text.txt", "w");
+    fclose(text_file);
+
+    int text = open("text.txt", 02);
     if (text == -1) {
-        perror("Unable to create/open text.txt");
-        return 1;
+        perror("Unable to open text.txt");
+        exit(1);
     }
+
+    long page_size = sysconf(_SC_PAGESIZE);
+    long chunk_size = 1024 * page_size;
+
+    if (ftruncate(text, MAX_FILE_SIZE) == -1) {
+        perror("ftruncate");
+        exit(1);
+    }
+
+    char *map = mmap(NULL, MAX_FILE_SIZE, 0x2, 0x01, text, 0);
+    if (map == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
     int rand = open("/dev/random", 00);
     if (rand == -1) {
-        perror("Unable to open /dev/random");
-        return 1;
+        perror("open");
+        munmap(map, MAX_FILE_SIZE);
+        exit(1);
     }
 
-    ftruncate(text, FILE_SIZE);
-
-    char *map = (char *) mmap(NULL, FILE_SIZE, 0x1 | 0x2, 0x01, text, 0);
-
-    int capital_letters_count = 0;
     char buffer[BUFFER_SIZE];
-    int char_count = 0;
-
-    while (char_count < FILE_SIZE) {
-        char symbol;
-
-        if (char_count % 1024 == 0) {
-            map[char_count++] = '\n';
-        } else if (read(rand, buffer, sizeof(buffer)) > 0) {
-            for (int i = 0; i < sizeof(buffer); i++) {
-                symbol = buffer[i];
-                if (char_count >= FILE_SIZE) {
-                    break;
-                }
-                if (isprint(symbol)) {
-                    if (isupper(symbol)) {
-                        capital_letters_count++;
-                        map[char_count++] = tolower(symbol);
-                    } else {
-                        map[char_count++] = symbol;
-                    }
-                }
+    int buffer_index = 0;
+    for (int i = 0; i < MAX_FILE_SIZE; i++) {
+        if (!buffer_index) {
+            ssize_t bytes_read = read(rand, buffer, BUFFER_SIZE);
+            if (bytes_read != -1) {
+                buffer_index = bytes_read;
+            } else {
+                perror("read");
+                munmap(map, MAX_FILE_SIZE);
+                exit(1);
             }
+        }
+
+        char symbol = buffer[--buffer_index];
+        isprint(symbol) ? map[i] = symbol : i--;
+
+        if (!(i % 1024)) {
+            map[i] = '\n';
         }
     }
 
-    printf("Total number of capital letters in the file is: %i\n", capital_letters_count);
+    int capital_count = 0;
+    for (off_t offset = 0; offset < MAX_FILE_SIZE; offset += chunk_size) {
+        off_t size = chunk_size;
+        if (offset + size > MAX_FILE_SIZE) {
+            size = MAX_FILE_SIZE - offset;
+        }
 
-    munmap(map, FILE_SIZE);
+        map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, text, offset);
+        if (map == MAP_FAILED) {
+            perror("mmap");
+            exit(1);
+        }
 
-    close(text);
+        for (off_t i = 0; i < size; i++) {
+            char c = map[i];
+            if (isupper(c)) {
+                capital_count++;
+                map[i] = tolower(c);
+            }
+        }
+
+        if (munmap(map, size) == -1) {
+            perror("munmap");
+            exit(1);
+        }
+    }
+
+    printf("Total capital letters: %d\n", capital_count);
+
     close(rand);
+    close(text);
 
     return 0;
 }
